09ccf831763a5aba74a9b984dcb6dce10e47ac95
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-06 11:25:05 -0500

    chore: Update login page background images and form styling

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BtnComponent } from '../../components/btn/btn.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [BtnComponent],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
