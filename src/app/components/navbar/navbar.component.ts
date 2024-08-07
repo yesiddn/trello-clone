d26727a407c28e71816794932c1fc6e3e9878ded
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-07 12:05:16 -0500

    feat: Add navbar component and CDK/overlay usage

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BtnComponent } from '../btn/btn.component';
+import { OverlayModule } from '@angular/cdk/overlay';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [BtnComponent, OverlayModule],
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent {
+  isOpen = false;
+}
