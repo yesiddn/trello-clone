aa86688b3a5d18daff198f1dce1ccd65e14b7f75
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-09 15:02:13 -0500

    feat: Add boards page content

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBox, faClock, faWaveSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FontAwesomeModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
-
+  faBox = faBox;
+  faTrello = faTrello;
+  faWaveSquare = faWaveSquare;
+  faClock = faClock;
 }
