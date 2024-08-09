c7706e107810c3b22e1432bb7e9b7e6e1459d8c5
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-09 16:43:04 -0500

    feat: Add accordion cdk usage

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBox, faClock, faWaveSquare } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleUp, faBorderAll, faBox, faClock, faGear, faHeart, faUsers, faWaveSquare } from '@fortawesome/free-solid-svg-icons';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule],
+  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
@@ -15,4 +16,11 @@ export class BoardsComponent {
   faTrello = faTrello;
   faWaveSquare = faWaveSquare;
   faClock = faClock;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUsers;
+  faGear = faGear;
+  faAngleDown = faAngleDown;
+  faAngleUp = faAngleUp;
+  accordionMenuIsExpanded = false;
 }
