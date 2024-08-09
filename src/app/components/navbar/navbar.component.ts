639445f8f5b2a390ddfb961b9cbe11e71879c14a
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-09 15:01:51 -0500

    feat: Add icons with Font Awesome

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { BtnComponent } from '../btn/btn.component';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell, faChevronDown, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [BtnComponent, OverlayModule],
+  imports: [BtnComponent, OverlayModule, FontAwesomeModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+  fabell = faBell
+  faInfoCircle = faInfoCircle
+  faChevronDown = faChevronDown
+
   isOpen = false;
   isWorkspaceOpen = false;
   isRecentOpen = false;
