1aacd909261c45a19b5a6110201d188b0134f0f4
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-14 17:17:56 -0500

    feat: Add Table CDK usage

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,19 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HttpClient } from '@angular/common/http';
-import { CurrencyPipe } from '@angular/common';
-import { ScrollingModule } from '@angular/cdk/scrolling';
 import { Product } from '../../model/product.model';
+import {CdkTableModule} from '@angular/cdk/table';
 
 @Component({
-  selector: 'app-scroll',
+  selector: 'app-table',
   standalone: true,
-  imports: [NavbarComponent, ScrollingModule, CurrencyPipe],
-  templateUrl: './scroll.component.html'
+  imports: [NavbarComponent, CdkTableModule],
+  templateUrl: './table.component.html'
 })
-export class ScrollComponent {
+export class TableComponent {
   private http: HttpClient = inject(HttpClient);
   products: Product[] = [];
+  columns: string[] = ['#No', 'Title', 'Price',  'Image']; // se tienen que guardar las columnas de la tabla en un array -> el nombre de las columnas no necesariamente tiene que ser el mismo que el de las propiedades del objeto
 
   ngOnInit() {
     this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products').subscribe(data => {
