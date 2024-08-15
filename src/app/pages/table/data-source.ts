56af89842d3ba7a97ca74020ec0ecc811eda6f7e
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-15 17:04:53 -0500

    feat: Add filters and debounce to table

--- src/app/pages/table/data-source.ts
+++ src/app/pages/table/data-source.ts
@@ -4,28 +4,40 @@ import { BehaviorSubject, Observable } from "rxjs";
 
 export class DataSourceProduct extends DataSource<Product> {
   data = new BehaviorSubject<Product[]>([]);
+  originalData: Product[] = [];
 
   connect(): Observable<Product[]> {
     return this.data;
   }
 
   init(products: Product[]) {
+    this.originalData = products;
     this.data.next(products); // asigna el valor de products al BehaviorSubject
   }
 
   getTotal() {
     const products = this.data.getValue(); // obtiene el valor del BehaviorSubject
-    return products.reduce((acc, product) => acc + product.price, 0)
+    return products.reduce((acc, product) => acc + product.price, 0);
   }
 
   update(id: Product['id'], changes: Partial<Product>) {
     const products = this.data.getValue();
-    const index = products.findIndex(product => product.id === id);
+    const index = products.findIndex((product) => product.id === id);
 
     if (index !== -1) {
       products[index] = { ...products[index], ...changes };
       this.data.next(products);
     }
+  }
+
+  find(query: string) {
+    const newProducts = this.originalData.filter(
+      (product) =>
+        product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()) ||
+        product.id == query ||
+        product.price === Number(query)
+    );
+    this.data.next(newProducts);
   }
 
   disconnect() {}
--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../../model/product.model';
@@ -6,11 +7,12 @@ import {CdkTableModule} from '@angular/cdk/table';
 import { CurrencyPipe, NgClass } from '@angular/common';
 import { DataSourceProduct } from './data-source';
 import { BtnComponent } from '../../components/btn/btn.component';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [NavbarComponent, BtnComponent, CdkTableModule, NgClass, CurrencyPipe],
+  imports: [NavbarComponent, BtnComponent, ReactiveFormsModule, CdkTableModule, NgClass, CurrencyPipe],
   templateUrl: './table.component.html'
 })
 export class TableComponent {
@@ -18,12 +20,23 @@ export class TableComponent {
   dataSourse = new DataSourceProduct();
   columns: string[] = ['#No', 'Title', 'Price',  'Image', 'Actions']; // se tienen que guardar las columnas de la tabla en un array -> el nombre de las columnas no necesariamente tiene que ser el mismo que el de las propiedades del objeto
   total = 0;
+  input = new FormControl('', {
+    nonNullable: true,
+  })
 
   ngOnInit() {
     this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products').subscribe(data => {
       this.dataSourse.init(data);
 
       this.total = this.dataSourse.getTotal();
+    });
+
+    this.input.valueChanges
+      .pipe(debounceTime(300)) // agrega un delay de 300ms para que no se vean los cambios cada que se escribe una letra
+      .subscribe(value => {
+        console.log(value);
+        this.dataSourse.find(value);
+        this.total = this.dataSourse.getTotal();
     });
   }
 
