3bf5fd84dbbe310ce1e29dc94a4ab3a4899c049d
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-19 15:27:52 -0500

    feat: Add auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@enviroment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private http = inject(HttpClient);
+  apiURL = environment.API_URL;
+
+  constructor() { }
+
+  login(email: string, password: string) {
+    this.http.post(`${this.apiURL}/api/v1/auth/login`, { email, password });
+  }
+}
