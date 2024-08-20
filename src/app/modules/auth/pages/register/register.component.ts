dc3aa5c8386bb72ee221b769083f1843d1c93018
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-20 14:19:04 -0500

    feat: Add register page

---
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { BackgroundComponent } from '../../components/background/background.component';
+import { NavbarComponent } from '../../../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { RegisterFormComponent } from '../../components/register-form/register-form.component';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [BackgroundComponent, NavbarComponent, HeaderComponent, FooterComponent, RegisterFormComponent],
+  templateUrl: './register.component.html'
+})
+export class RegisterComponent {
+
+}
