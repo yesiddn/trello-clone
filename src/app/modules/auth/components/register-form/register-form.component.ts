a689f79cfc344257229714a7532e6ede2f49cf86
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-20 15:05:20 -0500

    feat: Add register functionality

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -5,6 +5,8 @@ import { RequestStatus } from '../../../../model/request-status.model';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BtnComponent } from '../../../../components/btn/btn.component';
+import { AuthService } from '../../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-form',
@@ -14,6 +16,8 @@ import { BtnComponent } from '../../../../components/btn/btn.component';
 })
 export class RegisterFormComponent {
   private formBuilder = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
   form = this.formBuilder.nonNullable.group({
     name: ['', [Validators.required]],
@@ -28,12 +32,32 @@ export class RegisterFormComponent {
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  errorMessage: string = '';
 
   register() {
     if (this.form.valid) {
       this.status = 'loading';
+      this.errorMessage = '';
       const { name, email, password } = this.form.getRawValue();
-      console.log(name, email, password);
+      // console.log(name, email, password);
+
+      this.authService.register(name, email, password).subscribe({
+        next: () => {
+          this.status = 'success';
+          this.router.navigate(['/login']);
+        },
+        error: (error) => {
+          if (error.error.code === 'SQLITE_CONSTRAINT_UNIQUE'){
+            this.errorMessage = 'This user already exists.';
+          }
+
+          if (Array.isArray(error.error.message)) {
+            this.errorMessage = 'Invalid email.';
+          }
+          this.status = 'failed';
+          // console.log(error);
+        },
+      })
     } else {
       this.form.markAllAsTouched();
     }
