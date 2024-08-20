7a5266fa73f56548dab1429bb7e9f7cede5d4cec
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-20 14:17:18 -0500

    feat: Add header and footer components to login page

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { BackgroundComponent } from '../../components/background/background.component';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ BackgroundComponent, LoginFormComponent],
+  imports: [HeaderComponent, FooterComponent, BackgroundComponent, LoginFormComponent],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
