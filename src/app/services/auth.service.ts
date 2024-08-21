5b325bfeaaace944de1fedbef6960aaf2d68c307
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-21 16:11:21 -0500

    feat: Add password recovery and change password service methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -30,4 +30,12 @@ export class AuthService {
   isAvailable(email: string) {
     return this.http.post<{isAvailable: boolean}>(`${this.apiURL}/api/v1/auth/is-available`, { email });
   }
+
+  recovery(email: string) {
+    return this.http.post(`${this.apiURL}/api/v1/auth/recovery`, { email });
+  }
+
+  changePassword(token: string, newPassword: string) {
+    return this.http.post(`${this.apiURL}/api/v1/auth/change-password`, { token, newPassword });
+  }
 }
