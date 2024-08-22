8db519b62b7f8e9fbd360f75ecf5c5c610a4eea9
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-22 16:51:53 -0500

    feat: Update login method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@enviroment';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
+import { TokenService } from './token.service';
+import { ResponseLogin } from '../model/auth.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private http = inject(HttpClient);
+  private tokenService = inject(TokenService);
   apiURL = environment.API_URL;
 
   constructor() { }
 
   login(email: string, password: string) {
-    return this.http.post(`${this.apiURL}/api/v1/auth/login`, { email, password });
+    return this.http.post<ResponseLogin>(`${this.apiURL}/api/v1/auth/login`, { email, password })
+      .pipe(
+      // otro metodo de rxjs que nos permite hacer algo con la respuesta, en este caso guardar el token
+        // tap es un operador que nos permite hacer algo antes de devolver la respuesta sin afectar el flujo de datos
+        tap(response => {
+          this.tokenService.saveToken(response.access_token);
+        })
+      );
   }
 
   register(name: string, email: string, password: string) {
