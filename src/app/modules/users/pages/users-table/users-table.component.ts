fe8fd3976cbfba4513b56ba8ddcbafe1b556bf37
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-08-24 16:08:59 -0500

    feat: Update user table component

--- src/app/modules/users/pages/users-table/users-table.component.ts
+++ src/app/modules/users/pages/users-table/users-table.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DataSourceUser } from './data-source';
 import { CdkTableModule } from '@angular/cdk/table';
 import { NgClass } from '@angular/common';
+import { UsersService } from '../../../../services/users.service';
 
 @Component({
   selector: 'app-users-table',
@@ -10,29 +11,39 @@ import { NgClass } from '@angular/common';
   templateUrl: './users-table.component.html'
 })
 export class UsersTableComponent {
+  private userService = inject(UsersService);
+
   dataSource = new DataSourceUser();
   columns: string[] = ['id', 'avatar', 'name', 'email'];
 
   ngOnInit() {
-    this.dataSource.init([
-      {
-        id: '1',
-        name: 'User 1',
-        email: 'mail@mail.com',
-        avatar: 'https://api.lorem.space/image/face?w=150&h=150'
+    // this.dataSource.init([
+    //   {
+    //     id: '1',
+    //     name: 'User 1',
+    //     email: 'mail@mail.com',
+    //     avatar: 'https://api.lorem.space/image/face?w=150&h=150'
+    //   },
+    //   {
+    //     id: '2',
+    //     name: 'User 2',
+    //     email: 'mail2@mail.com',
+    //     avatar: 'https://api.lorem.space/image/face?w=150&h=150'
+    //   },
+    //   {
+    //     id: '3',
+    //     name: 'User 3',
+    //     email: 'mail3@mail.com',
+    //     avatar: 'https://api.lorem.space/image/face?w=150&h=150'
+    //   }
+    // ]);
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        this.dataSource.init(users);
       },
-      {
-        id: '2',
-        name: 'User 2',
-        email: 'mail2@mail.com',
-        avatar: 'https://api.lorem.space/image/face?w=150&h=150'
-      },
-      {
-        id: '3',
-        name: 'User 3',
-        email: 'mail3@mail.com',
-        avatar: 'https://api.lorem.space/image/face?w=150&h=150'
+      error: (err) => {
+        console.error(err);
       }
-    ]);
+    });
   }
 }
