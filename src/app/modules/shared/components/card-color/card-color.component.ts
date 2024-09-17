b8b9c2ff5045ccd25b6e50905329a0c435a3e87f
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-09-17 16:20:02 -0500

    Add card color component

---
+++ src/app/modules/shared/components/card-color/card-color.component.ts
@@ -0,0 +1,25 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-color',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './card-color.component.html'
+})
+export class CardColorComponent {
+  @Input() color: keyof typeof this.mapColors = 'sky';
+
+  mapColors = {
+    sky: 'bg-sky-500 hover:bg-sky-600 text-white',
+    yellow: 'bg-yellow-500 hover:bg-yellow-600 text-white',
+    green: 'bg-green-500 hover:bg-green-600 text-white',
+    red: 'bg-red-500 hover:bg-red-600 text-white',
+    violet: 'bg-violet-500 hover:bg-violet-600 text-white',
+    gray: 'bg-gray-500 hover:bg-gray-600 text-white',
+  };
+
+  get colors() {
+    return this.mapColors[this.color] ? this.mapColors[this.color] : this.mapColors.sky;
+  }
+}
