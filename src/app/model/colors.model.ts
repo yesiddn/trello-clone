545f84c19d6cb60f4dddaf563900aef7531e4920
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-09-18 09:58:37 -0500

    feat: Add dynamic colors model

---
+++ src/app/model/colors.model.ts
@@ -0,0 +1,16 @@
+export type Colors = 'sky' | 'yellow' | 'green' | 'red' | 'violet' | 'gray' | 'success' | 'danger' | 'primary' | 'gray-light';
+
+export type ObjectColors = Record<Colors, string>;
+
+export const COLORS: ObjectColors = {
+  sky: 'bg-sky-500 hover:bg-sky-600 text-white',
+  yellow: 'bg-yellow-500 hover:bg-yellow-600 text-white',
+  green: 'bg-green-500 hover:bg-green-600 text-white',
+  red: 'bg-red-500 hover:bg-red-600 text-white',
+  violet: 'bg-violet-500 hover:bg-violet-600 text-white',
+  gray: 'bg-gray-500 hover:bg-gray-600 text-white',
+  success: 'text-white bg-success-500 hover:bg-success-600 focus:ring-success-300',
+  danger: 'text-white bg-red-500 hover:bg-red-600 focus:ring-red-300',
+  primary: 'text-white bg-primary-500 hover:bg-primary-600 focus:ring-primary-300',
+  'gray-light': 'text-gray-700 bg-gray-200 hover:bg-gray-300 focus:ring-gray-100'
+}
