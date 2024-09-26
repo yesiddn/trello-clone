e708b0f64cfaed4eb1e35a2dc8696bce6fc5b326
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-09-26 18:32:35 -0500

    Refactor new board form component

--- src/app/modules/shared/components/new-board-form/new-board-form.component.ts
+++ src/app/modules/shared/components/new-board-form/new-board-form.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { BtnComponent } from "../../../../components/btn/btn.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-new-board-form',
   standalone: true,
-  imports: [ReactiveFormsModule, BtnComponent],
+  imports: [ReactiveFormsModule, BtnComponent, FontAwesomeModule],
   templateUrl: './new-board-form.component.html'
 })
 export class NewBoardFormComponent {
   private formBuilder = inject(FormBuilder);
 
+  faCheck = faCheck;
   form = this.formBuilder.group({
     title: [''],
     backgroundColor: ['']
