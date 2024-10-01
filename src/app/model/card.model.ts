e5c3b64092a4fe111606a9327b29cdcff2d8604b
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-10-01 14:32:54 -0500

    Refactor UpdateCardDto model

--- src/app/model/card.model.ts
+++ src/app/model/card.model.ts
@@ -4,15 +4,20 @@ import { List } from "./list.model";
 export interface Card {
   id: string;
   title: string;
-  description: string;
+  description?: string;
   position: number;
   list: List;
 }
 
-export interface UpdateCardDto {
-  title?: string;
-  description?: string;
-  position?: number;
-  listId?: List['id'];
-  boardid?: Board['id'];
+// export interface UpdateCardDto {
+//   title?: string;
+//   description?: string;
+//   position?: number;
+//   listId?: List['id'];
+//   boardid?: Board['id'];
+// }
+
+export interface UpdateCardDto extends Partial<Omit<Card, 'id' | 'list'>> {
+  listId?: number | string;
+  boardId?: string;
 }
